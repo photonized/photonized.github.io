c5f36ba88693739df2f18cb0626a3a7d38af6e2d
Vlad <vladjdk@gmail.com>
2020-12-22 17:37:28 -0500

    first *real* commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+var element = React.createElement('h1', {className: 'greeting'
+}, 'Vlad learns frontend :)')
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  element,
   document.getElementById('root')
 );
 
